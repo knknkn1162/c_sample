9cfaedb7b36df0e7eac49b896b371ad25946900c
knknkn <knknkn1162@gmail.com>
2018-07-21 12:08:18 +0900

    imple sleep1.c

---
+++ sleep1.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+#include <errno.h>
+#include <string.h>
+
+void signal_handler(int no);
+
+int main(void) {
+  struct sigaction sa;
+  int ret;
+
+  sa.sa_handler = signal_handler;
+  sa.sa_flags = SA_RESTART;
+
+  sigemptyset(&sa.sa_mask);
+  sigaction(SIGINT, &sa, 0);
+
+  while(1) {
+    ret = sleep(3600);
+    if(ret != 0 && errno == EINTR) { continue; }
+    break;
+  }
+
+  return 0;
+}
+
+void signal_handler(int no) {
+  char *mes = "signal get\n";
+  write(1, mes, strlen(mes));
+}
