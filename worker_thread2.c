eacbfdd7df7b6b6b39691f88f83d6a667ac4172e
knknkn <knknkn1162@gmail.com>
2018-07-28 19:33:32 +0900

    try thread_pool

---
+++ worker_thread2.c
@@ -0,0 +1,117 @@
+#include <stdlib.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+
+#define NUM_THREADS 5
+#define MAX_QUEUE_SIZE 100
+
+typedef void* (*p_routine_t)(void*);
+
+typedef struct tpool_work {
+  p_routine_t routine;
+  void* arg;
+  struct tpool_work* next;
+} tpool_work_t;
+
+typedef struct tpool {
+  pthread_t threads[NUM_THREADS];
+  int cur_queue_size;
+  tpool_work_t* queue_head;
+  tpool_work_t* queue_tail;
+  pthread_mutex_t queue_lock;
+  pthread_cond_t queue_not_empty;
+  pthread_cond_t queue_not_full;
+} tpool_t;
+
+int tpool_init(tpool_t*);
+int tpool_destroy(tpool_t*);
+void* tpool_thread(void* arg);
+void* add_work(tpool_t* p_tpool, p_routine_t routine, void* arg);
+
+void* sample_work(void* arg) {
+  int m = *(int*)arg;
+  fprintf(stdout, "%d\n", m);
+  return NULL;
+}
+
+int main(void) {
+  tpool_t tpool;
+  int m;
+  char buf[256];
+  if(tpool_init(&tpool) < 0) {
+    perror("tpool_init error");
+    exit(1);
+  }
+
+  while(1) {
+    fgets( buf, sizeof( buf ), stdin );
+
+    if( strlen( buf ) <= 1 ) {
+        continue;
+    }
+    if( memcmp( buf, "quit", 4 ) == 0 ) {
+        break;
+    }
+    m = atoi(buf);
+    add_work(&tpool, sample_work, (void*)&m);
+  }
+
+  tpool_destroy(&tpool);
+  return 0;
+}
+
+int tpool_init(tpool_t* p_tpool) {
+  int i;
+  p_tpool = (tpool_t*)malloc(sizeof(tpool_t));
+
+  p_tpool->cur_queue_size = 0;
+  p_tpool->queue_head = NULL;
+  p_tpool->queue_tail = NULL;
+  pthread_mutex_init(&p_tpool->queue_lock, NULL);
+  pthread_cond_init(&(p_tpool->queue_not_empty), NULL);
+  pthread_cond_init(&(p_tpool->queue_not_full), NULL);
+
+  for(i = 0; i < NUM_THREADS; i++) {
+    pthread_create(&(p_tpool->threads[i]), NULL, tpool_thread, (void*)p_tpool);
+  }
+  return 0;
+}
+
+int tpool_destroy(tpool_t* p_tpool) {
+  printf("graceful shutdown...\n");
+  return 0;
+}
+
+void* tpool_thread(void* arg) {
+  while(1) {
+    // TODO
+  }
+  return NULL;
+}
+
+void* add_work(tpool_t* p_tpool, p_routine_t routine, void* arg) {
+  tpool_work_t* workp;
+  pthread_mutex_lock(&p_tpool->queue_lock);
+  if(p_tpool->cur_queue_size == MAX_QUEUE_SIZE) {
+    pthread_cond_wait(&p_tpool->queue_not_full, &p_tpool->queue_lock);
+  }
+
+  workp = (tpool_work_t*)malloc(sizeof(tpool_work_t));
+  workp->routine = routine;
+  workp->arg = arg;
+  workp->next = NULL;
+
+  if(p_tpool->cur_queue_size == 0) {
+    p_tpool->queue_tail = p_tpool->queue_head = workp;
+    pthread_cond_signal(&p_tpool->queue_not_empty);
+  } else {
+    p_tpool->queue_tail = workp;
+    p_tpool->queue_tail->next = p_tpool->queue_head;
+  }
+
+  p_tpool->cur_queue_size++;
+  pthread_mutex_unlock(&p_tpool->queue_lock);
+  return NULL;
+}
