3f2399f929f55d7c169b8ae0f4e5f1e081dcfc42
knknkn <knknkn1162@gmail.com>
2018-07-31 20:33:39 +0900

    use sbrk

--- tlpi/etext.c
+++ tlpi/etext.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
+#include <unistd.h>
 
-extern void *end, *edata, *etext;
+extern char end, edata, etext;
 
 int main(void) {
-
+  void *p = sbrk(0);
+  printf("sbrk: %p\n", p);
   printf("end: %p\n", &end);
+  printf("sbrk - end: %ld\n", (char*)p - &end);
   printf("edata: %p\n", &edata);
   printf("etext: %p\n", &etext);
+
 }
