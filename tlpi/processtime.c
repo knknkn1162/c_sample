9dad6c3e0dcf9301a2f3faf76feb759f7f8b65d0
knknkn <knknkn1162@gmail.com>
2018-08-01 11:15:48 +0900

    use clock

---
+++ tlpi/processtime.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <sys/times.h>
+#include <time.h>
+#include <unistd.h>
+
+int main(void) {
+  struct tms t;
+  clock_t clockTime = clock();
+  printf("start clock: %ld\n", clockTime);
+  static long clockTicks = 0;
+  int i;
+
+  clockTicks = sysconf(_SC_CLK_TCK);
+  printf("CLOCKS_PER_SEC=%ld sysconf(_SC_CLK_TCK)=%ld\n\n", (long)CLOCKS_PER_SEC, sysconf(_SC_CLK_TCK));
+
+  printf("clockTicks: %ld\n", clockTicks);
+  clockTime = clock();
+  if(clockTime == -1) {
+    perror("clock");
+  }
+
+  printf("%ld, %.4f\n", (long)clockTime, (double)clockTime/CLOCKS_PER_SEC);
+
+  for(i = 0; i < 10000; i++) {
+    if(times(&t) == -1) {
+      perror("times");
+    }
+
+    if(i == 100) {
+      printf("%.2f %.2f\n", (double)t.tms_utime/clockTicks, (double)t.tms_stime/clockTicks); 
+    }
+  }
+  clockTime = clock() - clockTime;
+  printf("%ld clockTime\n", clockTime);
+
+  return 0;
+
+}
