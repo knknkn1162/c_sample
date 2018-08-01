5bb627ba08553ac1edadf7c6a05e39c3f704cef0
knknkn <knknkn1162@gmail.com>
2018-08-01 11:21:44 +0900

    fix process time sample

--- tlpi/processtime.c
+++ tlpi/processtime.c
@@ -21,13 +21,13 @@ int main(void) {
 
   printf("%ld, %.4f\n", (long)clockTime, (double)clockTime/CLOCKS_PER_SEC);
 
-  for(i = 0; i < 10000; i++) {
+  for(i = 0; i < 100000; i++) {
     if(times(&t) == -1) {
       perror("times");
     }
 
-    if(i == 100) {
-      printf("%.2f %.2f\n", (double)t.tms_utime/clockTicks, (double)t.tms_stime/clockTicks); 
+    if(i == 1 || i == 100000-1) {
+      printf("%.5f %.5f\n", (double)t.tms_utime/clockTicks, (double)t.tms_stime/clockTicks);
     }
   }
   clockTime = clock() - clockTime;
