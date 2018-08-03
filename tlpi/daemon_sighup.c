9b238ebdabafda0924ec6114b6a63a96c73693f3
knknkn1162 <knknkn1162@gmail.com>
2018-08-03 16:24:28 +0900

    daemon_sighup scaffold

---
+++ tlpi/daemon_sighup.c
@@ -0,0 +1,137 @@
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include <stdarg.h>
+#include <string.h>
+#include <signal.h>
+
+#define BD_NO_CHDIR 01
+#define BD_NO_CLOSE_FILES 02
+#define BD_NOREOPEN_STD_FDS 04
+#define BD_NO_UMASK0 010
+#define BD_MAX_CLOSE 8192
+
+int becomeDaemon(int flags);
+static void logMessage(const char *format, ...);
+static void logClose(void);
+static void sighupHandler(int sig);
+static void readConfigFile(const char *configFileName);
+
+static FILE *logfp;
+static const char *LOG_FILE = "/tmp/ds.log";
+static const char *CONFIG_FILE = "/tmp/ds.conf";
+
+static volatile sig_atomic_t hupReceived = 0;
+
+int main(void) {
+  return 0;
+}
+static void readConfigFile(const char *configFilename) {
+    FILE *configfp;
+#define SBUF_SIZE 100
+    char str[SBUF_SIZE];
+
+    configfp = fopen(configFilename, "r");
+    if (configfp != NULL) {                 /* Ignore nonexistent file */
+        if (fgets(str, SBUF_SIZE, configfp) == NULL)
+            str[0] = '\0';
+        else
+            str[strlen(str) - 1] = '\0';    /* Strip trailing '\n' */
+        logMessage("Read config file: %s", str);
+        fclose(configfp);
+    }
+}
+
+static void logClose(void)
+{
+    logMessage("Closing log file");
+    fclose(logfp);
+}
+
+static void sighupHandler(int sig) { hupReceived = 1; }
+
+static void logMessage(const char *format, ...)
+{
+    va_list argList;
+    const char *TIMESTAMP_FMT = "%F %X";        /* = YYYY-MM-DD HH:MM:SS */
+#define TS_BUF_SIZE sizeof("YYYY-MM-DD HH:MM:SS")       /* Includes '\0' */
+    char timestamp[TS_BUF_SIZE];
+    time_t t;
+    struct tm *loc;
+
+    t = time(NULL);
+    loc = localtime(&t);
+    if (loc == NULL ||
+           strftime(timestamp, TS_BUF_SIZE, TIMESTAMP_FMT, loc) == 0)
+        fprintf(logfp, "???Unknown time????: ");
+    else
+        fprintf(logfp, "%s: ", timestamp);
+
+    va_start(argList, format);
+    vfprintf(logfp, format, argList);
+    fprintf(logfp, "\n");
+    va_end(argList);
+}
+
+
+int becomeDaemon(int flags) {
+  int maxfd, fd;
+
+  printf("PID: %d\n", getpid());
+  switch(fork()) {
+    case -1: return -1;
+    case 0: break;
+    default: _exit(EXIT_SUCCESS);
+  }
+
+  if(setsid() == -1) {
+    return -1;
+  }
+
+  printf("%d\n", getpid());
+
+  switch(fork()) {
+    case -1: return -1;
+    case 0: break;
+    default: _exit(EXIT_SUCCESS);
+  }
+  printf("%d\n", getpid());
+
+  if(!(flags & BD_NO_UMASK0)) {
+    umask(0);
+  }
+
+  if(!(flags & BD_NO_CHDIR)) {
+    chdir("/");
+  }
+
+  if(!(flags & BD_NO_CLOSE_FILES)) {
+    maxfd = sysconf(_SC_OPEN_MAX);
+    if(maxfd == -1) {
+      maxfd = BD_MAX_CLOSE;
+    }
+
+    for(fd = 0; fd < maxfd; fd++) {
+      close(fd);
+    }
+  }
+
+
+  if(!(flags & BD_NOREOPEN_STD_FDS)) {
+    close(STDIN_FILENO);
+
+    fd = open("/dev/null", O_RDWR);
+    if(fd != STDIN_FILENO) { return -1; }
+    if(dup2(STDIN_FILENO, STDOUT_FILENO) != STDOUT_FILENO) {
+      return -1;
+    }
+    if(dup2(STDIN_FILENO, STDERR_FILENO) != STDERR_FILENO) {
+      return -1;
+    }
+  }
+
+  return 0;
+}
