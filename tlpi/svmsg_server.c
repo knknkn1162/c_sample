36eff09603dbe48c94b82b1fdf0afa75fdb163da
knknkn <knknkn1162@gmail.com>
2018-08-04 11:17:12 +0900

    try msgget & msgctl

---
+++ tlpi/svmsg_server.c
@@ -0,0 +1,30 @@
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <sys/stat.h>
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+
+#define KEY_FILE "./Dockerfile"
+
+int main(void) {
+  int msqid;
+  key_t key;
+  key = ftok(KEY_FILE, 1);
+  
+  while((msqid = msgget(key, IPC_CREAT | IPC_EXCL | S_IRUSR | S_IWUSR | S_IWGRP)) == -1) {
+    if(errno == EEXIST) {
+      msqid = msgget(key, 0);
+      msgctl(msqid, IPC_RMID, NULL);
+      printf("%d\n", msqid); 
+    } else {
+      perror("EExit");
+      exit(1);
+    }
+  }
+
+  exit(EXIT_SUCCESS);
+}
