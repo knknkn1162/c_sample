de8082b9e78fcc1b70143ffe7a87663e45f4db85
knknkn <knknkn1162@gmail.com>
2018-08-16 12:09:48 +0900

    SIGWINCH

---
+++ tlpi2/sigwinch.c
@@ -0,0 +1,33 @@
+#include <signal.h>
+#include <termios.h>
+#include <sys/ioctl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+static void handler(int sig) {}
+
+int main(int argc, char *argv[]) {
+  struct winsize ws;
+  struct sigaction sa;
+
+  sigemptyset(&sa.sa_mask);
+  sa.sa_flags = 0;
+  sa.sa_handler = handler;
+  if(sigaction(SIGWINCH, &sa, NULL) == -1) {
+    perror("sigaction");
+    exit(1);
+  }
+
+  while(1) {
+    pause();
+
+    if(ioctl(STDIN_FILENO, TIOCGWINSZ, &ws) == -1) {
+      perror("ioctl");
+    }
+    printf("Caught SIGWINCH, new window size:  %d * %d\n", ws.ws_row, ws.ws_col);
+  }
+
+  return 0;
+}
+
