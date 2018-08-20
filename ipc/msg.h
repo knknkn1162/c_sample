e21a45e68a56427ad9d20197060776fbabe85ff9
knknkn1162 <knknkn1162@gmail.com>
2018-08-20 16:43:43 +0900

    organize msg.h

--- ipc/msg.h
+++ ipc/msg.h
@@ -1,21 +1,18 @@
 #include <limits.h>
 #define SERVER_KEY 0x1aaaaaa1
 
-struct requestMsg {
-  long mtype;
+struct request {
   int clientId;
   char pathName[PATH_MAX];
 };
 
-#define REQ_MSG_SIZE (offsetof(struct requestMsg, pathName) - offsetof(struct requestMsg, clientId) + PATH_MAX)
-
 #define RESP_MSG_SIZE 8192
 
-struct responseMsg {
+struct response {
   long mtype;
   char data[RESP_MSG_SIZE];
 };
 
-#define RESP_MT_FAILURE 1
-#define RESP_MT_DATA 2
-#define RESP_MT_END 3
+#define RESP_FAILURE 1
+#define RESP_DATA 2
+#define RESP_END 3
