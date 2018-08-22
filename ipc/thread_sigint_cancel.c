892e8974fff5d3846037ddc1251bc838e247f883
knknkn <knknkn1162@gmail.com>
2018-08-22 22:22:42 +0900

    add cancellation implementation

---
+++ ipc/thread_sigint_cancel.c
@@ -0,0 +1,100 @@
+#include <pthread.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+#define BUF_SIZE 256
+
+void* reactor(void*);
+void* worker(void*);
+
+
+static pthread_mutex_t threadMutex = PTHREAD_MUTEX_INITIALIZER;
+static pthread_cond_t condBuf = PTHREAD_COND_INITIALIZER;
+static int count = 0;
+
+
+int main(int argc, char *argv[]) {
+  pthread_t pt;
+  sigset_t ss;
+  int sig;
+  void* res;
+  int flag = 1;
+
+  sigemptyset(&ss);
+  sigaddset(&ss, SIGINT);
+  sigprocmask(SIG_BLOCK, &ss, NULL);
+
+  pthread_create(&pt, NULL, &reactor, NULL);
+
+  sigemptyset(&ss);
+  sigaddset(&ss, SIGINT);
+  while(flag) {
+    printf("[main thread] sigwait ready..\n");
+    if(sigwait(&ss, &sig)) {
+      continue;
+    }
+    switch(sig) {
+      case SIGINT:
+        printf("[main thread] catch sigint\n");
+        flag = 0;
+        break;
+      default:
+        break;
+    }
+  }
+  printf("[main thread] exit\n");
+  if(pthread_cancel(pt) != 0) {
+    perror("pthread_cancel");
+    exit(1);
+  }
+
+  if(pthread_join(pt, &res) != 0) {
+    perror("pthread_join");
+    exit(1);
+  }
+  if(res == PTHREAD_CANCELED) {
+    printf("[main thread]: reactor was cancelled\n");
+  }
+
+  return 0;
+}
+
+
+
+void* reactor(void* arg) {
+  char buf[BUF_SIZE];
+  // do sth
+  while(1) {
+    pthread_t pt_worker;
+    fgets(buf, sizeof(buf), stdin);
+
+    pthread_mutex_lock(&threadMutex);
+    pthread_create(&pt_worker, NULL, &worker, (void*)buf);
+    // cancelation point -> mutex is already unlocked!
+    //  The pthread_cond_wait() function atomically blocks the current thread waiting on the condition variable specified by cond, and releases the mutex specified by mutex.
+    pthread_cond_wait(&condBuf, &threadMutex);
+    pthread_mutex_unlock(&threadMutex);
+    pthread_detach(pt_worker);
+  }
+
+  pthread_exit(NULL);
+}
+
+void* worker(void* arg) {
+  char buf[BUF_SIZE];
+
+  pthread_mutex_lock(&threadMutex);
+  strncpy(buf, (char*)arg, BUF_SIZE);
+  pthread_mutex_unlock(&threadMutex);
+  pthread_cond_signal(&condBuf);
+
+  // assume to take some time..
+  sleep(2);
+  pthread_mutex_lock(&threadMutex);
+  printf("[%d] > %s", count++, buf);
+  pthread_mutex_unlock(&threadMutex);
+  pthread_exit(NULL);
+}
