da4f449fa42bb8f9088e3fbfb240f9d6a2efb46f
knknkn <knknkn1162@gmail.com>
2018-08-23 11:18:19 +0900

    basic.c

--- shutdown/sig_atomic.c
+++ shutdown/sig_atomic.c
@@ -8,17 +8,31 @@
 
 #define BUF_SIZE 100
 
+volatile sig_atomic_t sigFlag = 0;
+void signal_handler(int sig) { sigFlag = 1; }
 long doCalc(long a);
 
 int main(int argc, char *argv[]) {
   char buf[BUF_SIZE];
   long num;
+  struct sigaction sa;
+  sigemptyset(&sa.sa_mask);
+  sa.sa_flags = 0;
+  sa.sa_handler = signal_handler;
+  if(sigaction(SIGINT, &sa, NULL) == -1) {
+    perror("sigaction");
+    exit(1);
+  }
   
   while(1) {
     // Strictly speaking, when the signal is caught, we don't know whether the function restarts or fails with error.
     // See also `Interruption of system calls and library functions by signal handlers` in `man 7 signal`.
     // But we assume that fgets use read syscall internally and fails with EINTR error, which is probably true.
     if(fgets(buf, BUF_SIZE, stdin) == NULL) {
+      if(sigFlag == 1 && errno == EINTR) {
+        printf("shutdown..\n");
+        break;
+      }
       perror("fgets");
       exit(1);
     }
