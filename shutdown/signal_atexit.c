7a425e6cda4d23e81126f663da6778e2ba590cd9
knknkn1162 <knknkn1162@gmail.com>
2018-08-23 17:00:05 +0900

    try atexit

---
+++ shutdown/signal_atexit.c
@@ -0,0 +1,49 @@
+#include <signal.h>
+#include <stdio.h>
+#include <string.h>
+#include <limits.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+
+#define BUF_SIZE 100
+
+long doCalc(long a);
+
+void exitHandler(void) {
+  printf("exit\n");
+}
+
+int main(int argc, char *argv[]) {
+  char buf[BUF_SIZE];
+  long num;
+
+  atexit(exitHandler);
+  
+  while(1) {
+    // Strictly speaking, when the signal is caught, we don't know whether the function restarts or fails with error.
+    // See also `Interruption of system calls and library functions by signal handlers` in `man 7 signal`.
+    // But we assume that fgets use read syscall internally and fails with EINTR error, which is probably true.
+    if(fgets(buf, BUF_SIZE, stdin) == NULL) {
+      perror("fgets");
+      exit(1);
+    }
+
+    if(strlen(buf) > 20) {
+      fprintf(stderr, "> [error] too big!\n");
+      continue;
+    }
+    if(buf[0] == '\n') {
+      continue;
+    }
+    
+    num = atol(buf);
+    printf("> %ld -> %ld\n", num, doCalc(num));
+  }
+
+  return 0;
+}
+
+long doCalc(long num) {
+  return num*2;
+}
