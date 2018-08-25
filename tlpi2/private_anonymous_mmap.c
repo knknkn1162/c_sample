76d8d835fbffd92b3bdcefa582c36838c4b2c4d8
knknkn <knknkn1162@gmail.com>
2018-08-25 17:44:19 +0900

    try MAP_FIXED

--- tlpi2/private_anonymous_mmap.c
+++ tlpi2/private_anonymous_mmap.c
@@ -18,11 +18,18 @@ int main(int argc, char *argv[]) {
     perror("mmap");
     exit(1);
   }
+  printf("addr: %p\n", addr);
 
   if((numRead = read(STDIN_FILENO, addr, BUF_SIZE)) < 0) {
     perror("write");
     exit(1);
   }
+  printf("%s\n", addr);
+
+  // If the memory  region  specified by  addr  and len overlaps pages of any existing mapping(s), then the overlapped part of the existing mapping(s) will be discarded.
+  /* addr = mmap(addr, numRead, PROT_WRITE | PROT_READ, MAP_PRIVATE | MAP_ANONYMOUS | MAP_FIXED, -1, 0); */
+  /* printf("addr: %p\n", addr); */
+  /* printf("%s\n", addr); // nothing */
 
   if(write(STDOUT_FILENO, addr, numRead) == -1) {
     perror("write");
