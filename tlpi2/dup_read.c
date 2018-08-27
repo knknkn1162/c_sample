61acabd4b4024d9d9d3e1dbffd475d8c9bcb6a9d
knknkn <knknkn1162@gmail.com>
2018-08-27 21:09:36 +0900

    dup

--- tlpi2/dup_read.c
+++ tlpi2/dup_read.c
@@ -8,20 +8,31 @@
 
 int main(void) {
   int fd = open("Dockerfile", O_RDONLY);
-  int numRead, res;
+  int numRead;
 
-  dup2(fd, STDOUT_FILENO);
+  dup2(fd, STDIN_FILENO);
 
   char buf[1024];
-  numRead = read(STDOUT_FILENO, buf, 1024);
+  numRead = read(STDIN_FILENO, buf, 1024);
 
-  fprintf(stderr, "> %s \n", buf);
+  //fprintf(stderr, "> %s \n", buf);
+  /*
   if(write(STDOUT_FILENO, buf, numRead) == -1) {
     if(errno == EBADF) {
       // 9 (Bad file descriptor)
       fprintf(stderr, "%d (%s)\n", errno, strerror(errno));
     }
-  }
+  }*/
 
+  if(close(fd) == -1) {
+    perror("close");
+    exit(1);
+  }
+  fprintf(stderr, "close fd\n");
+
+  // still fd
+  memcpy(buf, "ao\n", 4);
+  numRead = read(STDIN_FILENO, buf, 1024);
+  fprintf(stdout, buf, numRead);
 
 }
